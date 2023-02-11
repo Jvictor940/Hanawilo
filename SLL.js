aed71be418fe05f57bb4cbe648d563ab08a68856
Jerry Victor <jerryvictor@jerrys-mbp.myfiosgateway.com>
2023-02-11 15:24:07 -0500

    added pop and shift to linked list

--- SLL.js
+++ SLL.js
@@ -53,6 +53,42 @@ class SinglyLinkedList {
         }
         return removedNode;
     }
+
+    pop() {
+        if (!this.length) return; 
+
+        let current = this.head;
+        let newTail = current;
+
+        while (current.next){
+            newTail = current;
+            current = current.next;
+        }
+        this.tail = newTail;
+        this.tail.next = null;
+        this.length--;
+
+        if (!this.length) {
+            this.head = null;
+            this.tail = null; 
+        }
+        return current; 
+    }
+
+    unshift(val) {
+        let newNode = new Node(val);
+
+        if (!this.length){
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            newNode.next = this.head;
+            this.head = newNode; 
+        }
+        this.length++;
+
+        return this; 
+    }
 }
 
 
